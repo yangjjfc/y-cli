679e647413c320b930780e78b3de71461b66e6d5
yangjj <1542103461@qq.com>
2019-08-02 17:51:32 +0800

    update

--- vue-system-tpm/babel.config.js
+++ vue-system-tpm/babel.config.js
@@ -1,5 +1,12 @@
-module.exports = {
-  presets: [
-    '@vue/app'
-  ]
+const isProduction = process.env.NODE_ENV === 'production';
+let plugins = [];
+if (!isProduction) {
+    plugins.push('dynamic-import-node');
 }
+
+module.exports = {
+    presets: [
+        '@vue/app'
+    ],
+    plugins
+};
